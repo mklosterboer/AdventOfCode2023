13f53e1d5772379c65dc27a94d3b4fff27cba2f0
Michael <mklosterboer@gmail.com>
2023-12-04 21:26:58 -0600

    Actually update the gitignore this time...

---
+++ Problems/Day04Problem/Day04Problem.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2023.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Problems
+{
+    internal class Day04Problem : Problem
+    {
+        protected override string InputName => "Test";
+
+        public override object PartOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object PartTwo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 var stopwatch = Stopwatch.StartNew();
 
-IProblem problem = new Day03Problem();
+IProblem problem = new Day04Problem();
 
 var runner = new Runner(problem);
 
