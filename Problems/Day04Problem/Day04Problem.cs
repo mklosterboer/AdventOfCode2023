f2c54cf3e5b8d4f95f7a711b72514977327798fd
Michael <mklosterboer@gmail.com>
2023-12-04 22:19:04 -0600

    Complete Day 04

--- Problems/Day04Problem/Day04Problem.cs
+++ Problems/Day04Problem/Day04Problem.cs
@@ -1,24 +1,87 @@
 ﻿using AdventOfCode2023.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Problems
 {
-    internal class Day04Problem : Problem
+    internal partial class Day04Problem : Problem
     {
-        protected override string InputName => "Test";
+        protected override string InputName => "Actual";
+
+        private IEnumerable<string> Input { get; init; }
+
+        public Day04Problem()
+        {
+            Input = GetInputStringList();
+        }
 
         public override object PartOne()
         {
-            throw new NotImplementedException();
+            return Input
+                .Select(x => new Card(x))
+                .Sum(x => x.Points);
         }
 
         public override object PartTwo()
         {
-            throw new NotImplementedException();
+            var cards = Input
+                .Select(x => new Card(x))
+                .ToDictionary(x => x.Number);
+
+            for (var i = 1; i < cards.Count; i++)
+            {
+                var card = cards[i];
+
+                foreach (var instance in card.CardsWon)
+                {
+                    cards[instance].AddInstances(1 * card.InstanceCount);
+                }
+            }
+
+            return cards.Values.Sum(x => x.InstanceCount);
+        }
+
+        private partial class Card
+        {
+            public int Number { get; init; }
+
+            private int NumWinningNumbers { get; init; }
+
+            public int InstanceCount { get; private set; }
+
+            [GeneratedRegex(@"\d+")]
+            private static partial Regex NumberRegex();
+
+            public Card(string input)
+            {
+                InstanceCount = 1;
+
+                var splitString = input.Split(':');
+
+                Number = int.Parse(NumberRegex().Match(splitString[0]).Value);
+
+                var numberString = splitString[1].Split("|");
+
+                var winningNumbers = NumberRegex()
+                    .Matches(numberString[0])
+                    .Select(match => int.Parse(match.Value))
+                    .ToHashSet();
+
+                var myNumbers = NumberRegex()
+                     .Matches(numberString[1])
+                     .Select(match => int.Parse(match.Value))
+                     .ToHashSet();
+
+                NumWinningNumbers = winningNumbers.Intersect(myNumbers).Count();
+            }
+
+            public int Points => (int)Math.Pow(2, NumWinningNumbers - 1);
+
+            public IEnumerable<int> CardsWon => NumWinningNumbers == 0
+                ? []
+                : Enumerable.Range(Number + 1, NumWinningNumbers);
+
+            public void AddInstances(int instancesToAdd) =>
+                InstanceCount += instancesToAdd;
         }
     }
 }
