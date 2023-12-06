ad6adaba651e3addfb369a91f6dbd4ecbfefc5f1
Michael <mklosterboer@gmail.com>
2023-12-06 17:57:04 -0600

    Reimplement Day 05

--- Problems/Day05Problem/Day05Problem.cs
+++ Problems/Day05Problem/Day05Problem.cs
@@ -20,7 +20,6 @@ namespace AdventOfCode2023.Problems
             return seeds.Min(x => x.Location);
         }
 
-        // BRUTE FORCE - DON'T DO THIS - TOOK ~3 MINUTES with 8 cores
         public override object PartTwo()
         {
             var input = GetInputStringList().ToList();
@@ -31,44 +30,23 @@ namespace AdventOfCode2023.Problems
                 .Matches(GetFirstRow().Split(":")[1])
                 .Select(x => x.Value.Split(" ").Select(y => long.Parse(y)).ToArray());
 
-            var seedRanges = seedPairs.Select(x => new SeedRange(x[0], x[1]));
+            var seedRanges = seedPairs.Select(x => new LongRange(x[0], x[0] + x[1] - 1)).ToList();
 
-            object lockMinLocation = new();
-            var minLocation = long.MaxValue;
+            // Shamelessly "borrowed" from Kevin's solution
+            var soilRanges = SeedFactory.GetMapRanges(seedRanges, seedFactory.SeedToSoilMap);
+            var fertRanges = SeedFactory.GetMapRanges(soilRanges, seedFactory.SoilToFertMap);
+            var waterRanges = SeedFactory.GetMapRanges(fertRanges, seedFactory.FertToWaterMap);
+            var lightRanges = SeedFactory.GetMapRanges(waterRanges, seedFactory.WaterToLightMap);
+            var tempRanges = SeedFactory.GetMapRanges(lightRanges, seedFactory.LightToTempMap);
+            var humdityRanges = SeedFactory.GetMapRanges(tempRanges, seedFactory.TempToHumidityMap);
+            var locationRanges = SeedFactory.GetMapRanges(humdityRanges, seedFactory.HumidityToLocationMap);
 
-            Parallel.ForEach(seedRanges, seedRange =>
-            {
-                var localMinLocation = long.MaxValue;
-                var end = seedRange.Start + seedRange.Length;
-
-                long endCurrentRange = 1;
-
-                for (var i = seedRange.Start; i < end; i += endCurrentRange)
-                {
-                    var seed = seedFactory.Create(i);
-                    endCurrentRange = seed.RangeEnd;
-
-                    if (seed.Location < minLocation)
-                    {
-                        minLocation = seed.Location;
-                    }
-                }
-                lock (lockMinLocation)
-                {
-                    if (localMinLocation < minLocation)
-                    {
-                        minLocation = localMinLocation;
-                    }
-                }
-                Console.WriteLine($"Done {seedRange.Start} {seedRange.Length}");
-            });
-
-            return minLocation;
+            return locationRanges.MinBy(x => x.Start).Start;
         }
 
         private class Mapping
         {
-            private List<MapRange> Ranges { get; init; }
+            public List<MapRange> Ranges { get; init; }
 
             private static Regex MapRegex = new(@"\d+");
 
@@ -92,23 +70,25 @@ namespace AdventOfCode2023.Problems
 
                     var destinationStart = matches[0];
                     var sourceStart = matches[1];
-                    var length = matches[2];
+                    var length = matches[2] - 1;
 
                     Ranges.Add(new MapRange(sourceStart, destinationStart, length));
                 }
+
+                Ranges = Ranges.OrderBy(x => x.DestinationStart).ToList();
             }
 
-            public (long value, long endOfRange) GetMappedValue(long value)
+            public long GetMappedValue(long value)
             {
                 foreach (var range in Ranges)
                 {
                     if (TryGetDestination(range, value, out var destination))
                     {
-                        return (destination, range.SourceEnd);
+                        return destination;
                     }
                 }
 
-                return (value, 1);
+                return value;
             }
 
             public static bool TryGetDestination(MapRange range, long source, out long destination)
@@ -124,44 +104,125 @@ namespace AdventOfCode2023.Problems
             }
         }
 
-        private record Seed(long Id, long Soil, long Fert, long Water, long Light, long Temp, long Humidity, long Location, long RangeEnd);
+        private record Seed(long Id, long Soil, long Fert, long Water, long Light, long Temp, long Humidity, long Location);
 
         private class SeedFactory(List<string> input)
         {
-            private Mapping SeedToSoilMap { get; init; } = new Mapping(input, "seed-to-soil map:");
-            private Mapping SoilToFertMap { get; init; } = new Mapping(input, "soil-to-fertilizer map:");
-            private Mapping FertToWaterMap { get; init; } = new Mapping(input, "fertilizer-to-water map:");
-            private Mapping WaterToLightMap { get; init; } = new Mapping(input, "water-to-light map:");
-            private Mapping LightToTempMap { get; init; } = new Mapping(input, "light-to-temperature map:");
-            private Mapping TempToHumidityMap { get; init; } = new Mapping(input, "temperature-to-humidity map:");
-            private Mapping HumidityToLocationMap { get; init; } = new Mapping(input, "humidity-to-location map:");
+            public Mapping SeedToSoilMap { get; init; } = new Mapping(input, "seed-to-soil map:");
+            public Mapping SoilToFertMap { get; init; } = new Mapping(input, "soil-to-fertilizer map:");
+            public Mapping FertToWaterMap { get; init; } = new Mapping(input, "fertilizer-to-water map:");
+            public Mapping WaterToLightMap { get; init; } = new Mapping(input, "water-to-light map:");
+            public Mapping LightToTempMap { get; init; } = new Mapping(input, "light-to-temperature map:");
+            public Mapping TempToHumidityMap { get; init; } = new Mapping(input, "temperature-to-humidity map:");
+            public Mapping HumidityToLocationMap { get; init; } = new Mapping(input, "humidity-to-location map:");
 
             public Seed Create(long seedId)
             {
                 var soil = SeedToSoilMap.GetMappedValue(seedId);
-                var fert = SoilToFertMap.GetMappedValue(soil.value);
-                var water = FertToWaterMap.GetMappedValue(fert.value);
-                var light = WaterToLightMap.GetMappedValue(water.value);
-                var temp = LightToTempMap.GetMappedValue(light.value);
-                var humidity = TempToHumidityMap.GetMappedValue(temp.value);
-                var location = HumidityToLocationMap.GetMappedValue(humidity.value);
+                var fert = SoilToFertMap.GetMappedValue(soil);
+                var water = FertToWaterMap.GetMappedValue(fert);
+                var light = WaterToLightMap.GetMappedValue(water);
+                var temp = LightToTempMap.GetMappedValue(light);
+                var humidity = TempToHumidityMap.GetMappedValue(temp);
+                var location = HumidityToLocationMap.GetMappedValue(humidity);
 
-                // We can skip forward by the minimum value of the rest of any given the range.
-                // EX 1: If fert.endOfRange = 1, it was not in a supplied range, so we cannot skip anything
-                // EX 2: If fert.endOfRange = 10 and water.endOfRange = 20 and the rest are >20, we can skip the next 10 seeds because
-                //       the next 10 ferts will produce a larger water number and everything else will also continue sequentially up.
-                //       Therefore, none of the next 10 seeds are going to produce a location smaller than this one.
-                long[] endOfRange = [soil.endOfRange, fert.endOfRange, water.endOfRange, light.endOfRange, temp.endOfRange, humidity.endOfRange, location.endOfRange];
+                return new Seed(seedId, soil, fert, water, light, temp, humidity, location);
+            }
 
-                return new Seed(seedId, soil.value, fert.value, water.value, light.value, temp.value, humidity.value, location.value, endOfRange.Min());
+            public static List<LongRange> GetMapRanges(List<LongRange> source, Mapping destination)
+            {
+                var result = new List<LongRange>();
+
+                foreach (var sourceRange in source.OrderBy(x => x.Start))
+                {
+                    var sourceRanges = new List<LongRange>();
+                    var localDestinationRanges = new List<LongRange>();
+
+                    foreach (var destinationRange in destination.Ranges.OrderBy(x => x.SourceStart))
+                    {
+                        if (sourceRange.Start > destinationRange.SourceEnd)
+                        {
+                            // This range does not overlap at all
+                            continue;
+                        }
+                        if (sourceRange.Start >= destinationRange.SourceStart && sourceRange.End <= destinationRange.SourceEnd)
+                        {
+                            // Source range is entirely within destination range
+                            var startOffset = sourceRange.Start - destinationRange.SourceStart;
+                            var endOffset = destinationRange.SourceEnd - sourceRange.End;
+
+                            localDestinationRanges.Add(new LongRange(destinationRange.DestinationStart + startOffset, destinationRange.DestinationEnd - endOffset));
+                            sourceRanges.Add(sourceRange);
+
+                            // No need to continue, we covered the entire source range.
+                            break;
+                        }
+
+                        if (sourceRange.Start > destinationRange.SourceStart && sourceRange.Start <= destinationRange.SourceEnd && sourceRange.End > destinationRange.SourceEnd)
+                        {
+                            // Source overlaps end of destination range
+                            var overlapAmount = destinationRange.SourceEnd - sourceRange.Start;
+
+                            localDestinationRanges.Add(new LongRange(destinationRange.DestinationEnd - overlapAmount, destinationRange.DestinationEnd));
+                            sourceRanges.Add(new LongRange(sourceRange.Start, destinationRange.SourceEnd));
+                        }
+                        else if (sourceRange.Start <= destinationRange.SourceStart && sourceRange.End >= destinationRange.SourceEnd)
+                        {
+                            // Destination range is entirely within source range
+                            localDestinationRanges.Add(new LongRange(destinationRange.DestinationStart, destinationRange.DestinationEnd));
+                            sourceRanges.Add(new LongRange(destinationRange.SourceStart, destinationRange.SourceEnd));
+                        }
+                        else if (sourceRange.Start < destinationRange.SourceStart && sourceRange.End >= destinationRange.SourceStart && sourceRange.End < destinationRange.SourceEnd)
+                        {
+                            // Source overlaps start of destination range
+                            var overlapAmount = sourceRange.End - destinationRange.SourceStart;
+
+                            localDestinationRanges.Add(new LongRange(destinationRange.DestinationStart, destinationRange.DestinationStart + overlapAmount));
+                            sourceRanges.Add(new LongRange(destinationRange.SourceStart, sourceRange.End));
+                        }
+                        else if (sourceRange.End < destinationRange.SourceStart)
+                        {
+                            // No more overlap to check
+                            break;
+                        }
+                    }
+
+                    var sourceCopy = sourceRange with { };
+                    foreach (var range in sourceRanges.OrderBy(x => x.Start))
+                    {
+                        if (sourceCopy.Start < range.Start)
+                        {
+                            localDestinationRanges.Add(new LongRange(sourceCopy.Start, range.Start - 1));
+                            sourceCopy = sourceCopy with { Start = range.End + 1 };
+                        }
+                        else if (sourceCopy.Start == range.Start && sourceCopy.End != range.End)
+                        {
+                            sourceCopy = sourceCopy with { Start = range.End + 1 };
+                        }
+                        else if (sourceCopy.End > range.End)
+                        {
+                            localDestinationRanges.Add(new LongRange(range.End + 1, sourceCopy.End));
+                        }
+                    }
+
+                    if (localDestinationRanges.Count == 0)
+                    {
+                        localDestinationRanges.Add(sourceRange);
+                    }
+
+                    result.AddRange(localDestinationRanges);
+                }
+
+                return result;
             }
         }
 
         private record MapRange(long SourceStart, long DestinationStart, long Length)
         {
             public long SourceEnd { get; init; } = SourceStart + Length;
+            public long DestinationEnd { get; init; } = DestinationStart + Length;
         }
 
-        private record SeedRange(long Start, long Length);
+        private record LongRange(long Start, long End);
     }
 }
